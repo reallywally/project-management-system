a1d2a81e8e9dae3c82ad0de843a9c718a5c4653c
wally <goldenale5500@gmail.com>
2024-07-09 00:23:51 +0900

    스프링 시큐리티 기본 설정

--- src/main/java/com/wally/config/AppConfig.java
+++ src/main/java/com/wally/config/AppConfig.java
@@ -7,13 +7,15 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
-import java.sql.Array;
 import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -31,19 +33,29 @@ public class AppConfig {
     }
 
     private CorsConfigurationSource corsConfigurationSource() {
-       return  new CorsConfigurationSource() {
-           @Override
-           public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-               CorsConfiguration cfg = new CorsConfiguration();
-               cfg.setAllowedOrigins(Arrays.asList(
-                       "http://localhost:3000/",
-                       "http://localhost:5173/",
-                       "http://localhost:4200/"
-               ));
+        return new CorsConfigurationSource() {
+            @Override
+            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                CorsConfiguration cfg = new CorsConfiguration();
+                cfg.setAllowedOrigins(Arrays.asList(
+                        "http://localhost:3000/",
+                        "http://localhost:5173/",
+                        "http://localhost:4200/"
+                ));
+                cfg.setAllowedMethods(Collections.singletonList("*"));
+                cfg.setAllowCredentials(true);
+                cfg.setAllowedHeaders(Collections.singletonList("*"));
+                cfg.setExposedHeaders(Arrays.asList("Authorization"));
+                cfg.setMaxAge(3600L);
 
+                return cfg;
+            }
+        };
+    }
 
-           }
-       }
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
---
+++ src/main/java/com/wally/config/JwtConstant.java
@@ -0,0 +1,7 @@
+package com.wally.config;
+
+public class JwtConstant {
+    public static final String SECRET_KEY = "Actually, this is random string for secret key";
+
+    public static final String JWT_HEADER = "Authorization";
+}

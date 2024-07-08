f02a77470b31a9dc8e65a0c95151fc927db5a445
wally <goldenale5500@gmail.com>
2024-07-08 23:56:03 +0900

    스프링 시큐리티 기본 설정 중

---
+++ src/main/java/com/wally/config/AppConfig.java
@@ -0,0 +1,49 @@
+package com.wally.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.sql.Array;
+import java.util.Arrays;
+
+@Configuration
+@EnableWebSecurity
+public class AppConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.sessionManagement(Management -> Management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(Authorize -> Authorize.requestMatchers("/api/**").authenticated()
+                        .anyRequest().permitAll())
+                .addFilterBefore(new JwtTokenValidator(), BasicAuthenticationFilter.class)
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()));
+        return http.build();
+    }
+
+    private CorsConfigurationSource corsConfigurationSource() {
+       return  new CorsConfigurationSource() {
+           @Override
+           public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+               CorsConfiguration cfg = new CorsConfiguration();
+               cfg.setAllowedOrigins(Arrays.asList(
+                       "http://localhost:3000/",
+                       "http://localhost:5173/",
+                       "http://localhost:4200/"
+               ));
+
+
+           }
+       }
+    }
+
+}
